ada853b81d53823d1e432cc1f688f9e65b175c00
erpalmon <erpalmon@myseneca.ca>
2025-09-20 18:25:12 -0400

    Modified src/ files and env files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,14 @@ import pluginJs from '@eslint/js';
 
 export default [
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: globals.node } },
+  {     languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
+
+
