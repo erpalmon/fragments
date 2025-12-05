3eaf318c7e38c34acbe014b0675e2e629ecf89e9
erpalmon <erpalmon@myseneca.ca>
2025-12-05 09:47:02 -0500

    fix

---
+++ jest.config.cjs
@@ -0,0 +1,25 @@
+// jest.config.cjs
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(js|mjs)$': ['babel-jest', { configFile: './babel.config.cjs' }],
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(node-fetch|data-uri-to-buffer|formdata-polyfill|fetch-blob|formdata-polyfill)/)',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  testMatch: ['**/tests/unit/**/*.test.@(js|mjs)'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/auth/(.*)$': '<rootDir>/src/auth/$1',
+    '^@/model/(.*)$': '<rootDir>/src/model/$1',
+    '^(\\..*)\\.js$': '$1',
+  },
+  moduleFileExtensions: ['js', 'mjs', 'json'],
+  testEnvironmentOptions: {
+    TEST_AUTH: process.env.TEST_AUTH || 'basic',
+  },
+  testTimeout: 30000,
+  verbose: true,
+};
+
